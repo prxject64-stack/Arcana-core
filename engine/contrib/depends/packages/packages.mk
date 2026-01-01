e578c0a349f4c86f684a82753278e3eeac86ad1b
Chapo64 <51744520-prxject64@users.noreply.replit.com>
2026-01-01 16:32:48 +0000

    Installed Monero Core Engine

---
+++ engine/contrib/depends/packages/packages.mk
@@ -0,0 +1,30 @@
+native_packages := native_protobuf
+packages := boost openssl zeromq unbound sodium protobuf
+
+ifneq ($(host_os),android)
+packages += libusb
+endif
+
+ifneq ($(host_os),freebsd)
+ifneq ($(host_os),android)
+packages += hidapi
+endif
+endif
+
+ifneq ($(host_os),mingw32)
+packages += ncurses readline
+endif
+
+linux_native_packages :=
+linux_packages :=
+
+freebsd_native_packages := freebsd_base
+freebsd_packages :=
+
+ifneq ($(build_os),darwin)
+darwin_native_packages := darwin_sdk
+endif
+darwin_packages :=
+
+android_native_packages := android_ndk
+android_packages :=
